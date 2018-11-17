8eafcd93d7e6aa71edb106a33180e9f07c4cc51d
Blandford Alex <alex.blandford@hetic.net>
2018-11-17 21:12:04 +0100

    removed js

--- index.js
+++
@@ -1,4 +0,0 @@
-const page = document.querySelector('body');
-const colors = ["blue", "yellow", "cyan", "red", "black", "pink", "green", "grey"];
-
-page.style.background = colors[Math.floor(Math.random() * colors.length)];
