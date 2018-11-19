3b5d94d7b913020ba4df3b633701677c1c0aaf18
Blandford Alex <alex.blandford@hetic.net>
2018-11-19 12:47:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-const page = document.querySelector('body');
-const colors = ["blue", "yellow", "cyan", "red", "black", "pink", "green", "grey"];
 
-page.style.background = colors[Math.floor(Math.random() * colors.length)];
